52ff84e586d6ac638d436fa9d5ae14c4e820497b
Rahul Kumar <rahul.kumar@above-inc.com>
2017-04-13 11:09:46 +0530

    Added interceptor like functionalities like showing busy when requesting

--- my-ionic-app/src/app/services/reddits.services.ts
+++ my-ionic-app/src/app/services/reddits.services.ts
@@ -1,29 +1,72 @@
 import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, RequestMethod, RequestOptionsArgs, Headers} from '@angular/http';
 import 'rxjs/Rx'
+import { NavController, LoadingController } from 'ionic-angular/index';
+
 
 @Injectable()
 export class RedditsService{
 	http: any;
 	baseUrl: string;
-	testUrl: string;
-	public loading;
-
-	constructor(http: Http){
-		
+	constructor(http: Http, public loadingCtrl : LoadingController){		
 		this.http = http;
 		this.baseUrl = "https://www.reddit.com/r";
-		this.testUrl = "http://studentsofstrength.com/api/user/find/?emailid=rahul.kumar@above-inc.com"
-
 	}
 
 	getPosts(category, limit){
 		return this.http.get(this.baseUrl+'/'+category+'/top.json?limit='+limit).map(res => res.json());
 	}
 
-	getTestUrl(){
-		return this.http.get(this.testUrl).map(res => res.json());
+	private showLoader() {
+		let loader = this.loadingCtrl.create({
+			content: "Fetching details..."
+		});
+		loader.present();
+		return loader;
 	}
+
+	private getHttpRequest(url: string, requestMethod: RequestMethod, body?: any) {
+	    return new Promise((resolve, reject) => {
+		      let requestOptionArgs: RequestOptionsArgs;
+		      requestOptionArgs = {
+		        url: url,
+		        method: requestMethod,
+		        body: body,
+		        headers: new Headers({
+		          "Content-Type": "application/json"
+		        })
+	          }  
+	        let loader = this.showLoader();
+	        
+	        this.http.request(url, requestOptionArgs)
+	        .map(res => res.json())
+	        .subscribe(data => {
+	          // we've got back the raw data, now generate the core schedule data
+	          // and save the data for later reference
+
+	          // Dismiss the loader and return response back.
+	          loader.dismiss().then(() => resolve(data));
+
+	        }, (error) => {
+	          // Dismiss the loader and return error back.
+	          loader.dismiss().then(() => reject(error));
+	        });    
+       });
+    }
+
+	public callGetRequest(url: string) {
+		return this.getHttpRequest(url, RequestMethod.Get);
+	}
+
+	public callPutRequest(url: string, body?: any) {
+		return this.getHttpRequest(url, RequestMethod.Put, body);
+	}
+
+	public callPostRequest(url: string, body?: any) {
+		return this.getHttpRequest(url, RequestMethod.Post, body);
+	}
+
+	
 }
 
 
--- my-ionic-app/src/pages/about/about.ts
+++ my-ionic-app/src/pages/about/about.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { RedditsService } from '../../app/services/reddits.services';
 
 
@@ -11,20 +11,13 @@ import { RedditsService } from '../../app/services/reddits.services';
 })
 export class AboutPage {  
 
-  public loading;
-  constructor(public navCtrl: NavController, private redditsService : RedditsService, private loadingCtrl: LoadingController ) {
-    console.log("About"+redditsService);
-    // Create the popup
-    this.loading = this.loadingCtrl.create({
-      content: 'Loading ...'
+  public loading;  
+  constructor(public navCtrl: NavController, private redditsService : RedditsService ) {}
+
+  ionViewDidLoad() {
+      this.redditsService.callGetRequest("https://api.github.com/").then((response)=>{
+      this.loading = response;
     });
-  }
-   ionViewDidLoad() {
-    this.loading.present();
-    this.redditsService.getTestUrl().subscribe( response => {
-  		console.log(response);
-		this.loading.dismiss();  		        
-  	})
   }
 
 }
